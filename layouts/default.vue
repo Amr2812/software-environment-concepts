e1d7ba2ba2376f8caa862f4b5b11ee8617fe068e
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-04-18 05:28:18 -0700

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-5xl text-primary-100">
+      software-environment-expressions
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
