b0f6dc9929e6b12baf2443205472b4a7dc68ab9e
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-04-30 18:16:42 -0700

    feat(PWA): check standalone or from browser

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
-    <Intro />
-    <Features />
-    <hr />
+    <div v-if="!isPWA">
+      <Intro />
+      <Features />
+      <hr />
+    </div>
     <Search />
-    <hr>
+    <hr />
     <Modules />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPWA: false,
+    };
+  },
+  mounted() {
+    // For IOS
+    if (window.navigator.standalone) this.isPWA = true;
+
+    // For Android
+    if (window.matchMedia("(display-mode: standalone)").matches)
+      this.isPWA = true;
+  },
+};
+</script>
