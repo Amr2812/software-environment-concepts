07c56ae4b6ff43dd6425aefbdeae424cb8f6d179
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-05-02 03:50:39 -0700

    feat: 404 page

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  generate: {
+    fallback: "404.html"
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Software Environment Concepts",
---
+++ pages/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white dark:bg-dark-default dark:text-gray-300 text-center">
+    <h1 class="text-red-800 text-9xl">
+      404
+    </h1>
+    <h2 class="text-2xl">
+      An Error Occured
+    </h2>
+    <nuxt-link to="/">
+      <button class="text-red-500 mt-2">
+        Homepage
+      </button>
+    </nuxt-link>
+  </div>
+</template>
