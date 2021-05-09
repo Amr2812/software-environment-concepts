ff6759c1d8ed2b23f7eccc94403323216cd1d5e2
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-05-09 13:53:45 -0700

    refactor: features

--- components/Features.vue
+++ components/Features.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="mt-20">
+  <section class="mt-20 grid lg:grid-cols-3">
     <a
       target="_blank"
       href="https://github.com/Amr2812/software-environment-concepts/"
-      class="btn flex items-center block px-4 text-gray-700 dark:text-gray-100 mb-6"
+      class="btn flex flex-col items-center block px-4 text-gray-700 dark:text-gray-100 mb-6"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +15,7 @@
           d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"
         />
       </svg>
-      <span class="ml-5">
+      <span class="mt-4">
         <h3 class="text-2xl capitalize">
           The book is Open Soure so the book is updated and mantained by various
           developers and if you are a developer feel free to contribute
@@ -23,7 +23,7 @@
       </span>
     </a>
     <button
-      class="btn flex items-center block px-4 py-2 text-gray-700 dark:text-gray-100 mb-6 text-left"
+      class="btn flex flex-col items-center block px-4 py-2 text-gray-700 dark:text-gray-100 mb-6 text-left"
       @click="install"
     >
       <svg
@@ -40,7 +40,7 @@
           d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
         ></path>
       </svg>
-      <span class="ml-5">
+      <span class="mt-4">
         <h3 class="text-2xl capitalize">
           The website is an Offline SSG PWA (Static Site Generated Progressive
           Web App) you can install on your device
@@ -49,7 +49,7 @@
     </button>
     <a
       href="#search"
-      class="btn flex items-center block px-4 py-2 text-gray-700 dark:text-gray-100 mb-6 text-left"
+      class="btn flex flex-col items-center block px-4 py-2 text-gray-700 dark:text-gray-100 mb-6 text-left"
     >
       <svg
         class="w-20 h-20"
@@ -65,7 +65,7 @@
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         ></path>
       </svg>
-      <span class="ml-5">
+      <span class="mt-4">
         <h3 class="text-2xl capitalize">
           You can search for any expression or page by using the search bar down
           below
