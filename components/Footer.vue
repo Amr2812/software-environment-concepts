1329fac4ebc7b5fe1d8d15ad7e5145b9f333af94
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-10-16 15:07:13 +0200

    update twitter to linkedin

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-dark-light text-center py-5 mt-3">
     <h4 class="text-lg mb-3">
       Created by
-      <a href="https://twitter.com/AmrElmohamady2">Amr Elmohamady</a> as
+      <a href="https://www.linkedin.com/in/amr-elmohamady-06342a1ba">Amr Elmohamady</a> as
       <a href="https://github.com/Amr2812/software-environment-concepts"
         >open-source</a
       >
